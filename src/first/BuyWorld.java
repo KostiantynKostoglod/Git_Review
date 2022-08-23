eb1dbac51548b4f8e84612c5e79714c7fdc33123
Kostiantyn Kostoglod <0675053366@ukr.net>
2022-08-23 11:34:21 -0400

    Project is set up. Gitignore file is added.

---
+++ src/first/BuyWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class BuyWorld {
+    public static void main(String[] args) {
+        System.out.println("5 wekks of java left");
+        System.out.println("buy world then");
+    }
+}
